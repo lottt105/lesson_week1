0b9e256418c909d17fca937046057237edfb43b8
lottt105 <hyobin0601@naver.com>
2023-07-13 14:27:00 +0900

    JS 실습 - 홀수, 짝수, 배열내 최대값

---
+++ js/practice.js
@@ -0,0 +1,17 @@
+// 1. 조건문을 이용해서 홀수와 짝수 비교하기
+//오브젝트 내에 인자를 받는 함수를 만들어서
+//짝수인지 홀수인지 판별
+let IsOddEven = {
+  getResult: (num) => (num % 2 === 0 ? "짝수" : "홀수"),
+};
+
+console.log(IsOddEven.getResult(3));
+console.log(IsOddEven.getResult(4));
+
+// 2. 배열내 최대값 찾기
+//배열을 인자로 받는 함수를 만들어서
+//그 배열의 최대값 반환
+const getMax = (arr) => Math.max(...arr);
+
+console.log(getMax([1, 2, 3, 4]));
+console.log(getMax([5, 3, 9, 3, 10]));
