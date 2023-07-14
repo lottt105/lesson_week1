d884ddbb6d4b735599df1be1cf1708e14114a8f7
lottt105 <hyobin0601@naver.com>
2023-07-14 16:09:31 +0900

    JS실습-반복문 원하는 데이터 출력

--- js/practice3.js
+++ js/practice3.js
@@ -14,3 +14,15 @@ title.addEventListener("click", (e) => {
   title.style.color = generateRandomColor();
   title.style.borderColor = generateRandomColor();
 });
+
+//실습 : 반복문을 이용해서 원하는 데이터를 출력하기(1)
+let data = [
+  { 회원번호: 0, 아이디: "red", 성별: "남자", 나이: 21 },
+  { 회원번호: 1, 아이디: "yellow", 성별: "남자", 나이: 17 },
+  { 회원번호: 2, 아이디: "orange", 성별: "여자", 나이: 16 },
+  { 회원번호: 3, 아이디: "green", 성별: "여자", 나이: 55 },
+];
+
+for (let d of data) {
+  if (d.성별 === "여자" && d.나이 >= 20) console.log(d);
+}
